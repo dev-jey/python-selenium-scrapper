b3f40edf06f874c2f862564380fb2873c04ecfbc
dev-jey <savalijey@gmail.com>
2021-01-27 12:16:27 +0300

    Add data to excel file

--- README.md
+++ README.md
@@ -3,5 +3,22 @@
 A simple scrapper to get data from top companies tables in wikipedia
 
 ### Technologies Used
-
+'''
 Selenium
+'''
+
+### How To Set Up
+1. Create a virtual env
+'''
+virtualenv -p python3 env
+'''
+
+2. Install Requirements
+'''
+pip3 install -r requirments.txt
+'''
+
+3. Run the app
+'''
+python3 scrap.py
+'''
--- scrap.py
+++ scrap.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 import json
+import pandas as pd
 import urllib.request
 from selenium.webdriver.chrome.options import Options
 
@@ -20,38 +21,43 @@ options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--no-sandbox")
 # options.add_argument("--headless")
-options.add_argument("--disable-setuid-sandbox") 
+options.add_argument("--disable-setuid-sandbox")
 options.add_argument("--remote-debugging-port=9222")
 options.add_argument("--disable-dev-shm-usage")
-
-
-
-
 
 
 class Scrap(object):
     def __init__(self):
         self.driver = webdriver.Chrome(
-                    ChromeDriverManager().install(), options=options)
+            ChromeDriverManager().install(), options=options)
         self.wait = WebDriverWait(self.driver, 30)
 
-
-
     def scrap_data(self):
-        self.driver.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+        self.driver.get(
+            'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
         self.wait.until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/thead/tr/th[1]/a")))
-        rows = self.driver.find_element_by_tag_name('table').find_elements_by_tag_name("tr")
-        data_items = []
+        rows = self.driver.find_element_by_id(
+            'constituents').find_elements_by_tag_name("tr")
+
         attributes = ['Symbol', 'Security', 'SEC Filings', 'GIS Sector', 'GIS Sub Industry', 'Head Qaurters location',
-                  'Date Added', 'CIK', 'Founded']
+                      'Date Added', 'CIK', 'Founded']
+
+        all_data = []
+
         for row_item in rows:
+            data_items = []
             for td in row_item.find_elements_by_tag_name("td"):
-                print(td)
                 data_items.append(''.join(td.text.split(',')))
+                detail = dict(zip(attributes, data_items))
+                all_data.append(detail)
+        df = pd.DataFrame.from_dict(all_data)
+        df.dropna(inplace=True)
         
-        print(data_items)
-    
+        writer = pd.ExcelWriter('companies.xlsx', engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet2', index=False)
+        writer.save()
+
 
 scrapper = Scrap()
 scrapper.scrap_data()
